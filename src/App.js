a85d365031ac371acec2d583fb0883b2a32cf091
Chris Moore <cwm322@gmail.com>
2016-11-26 19:46:49 +0000

    The start of greatness

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    render 
+
+    render() {
+
+        return (
+            <div className="app">
+                <div className="app-header">
+                    <div id="logo">CM</div>
+                    <h2>Christopher Moore</h2>
+                    <p className="app-intro">
+                        London based Software Engineer
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
